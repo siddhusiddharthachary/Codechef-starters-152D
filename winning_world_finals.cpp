f8c024d650b8242ac931089d01fb3d42a37b334d
siddhusiddharthachary <151196424+siddhusiddharthachary@users.noreply.github.com>
2024-09-19 20:44:49 +0530

    Create winning_world_finals.cpp

---
+++ winning_world_finals.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--) {
+        int M, P, cnt = 0;
+        cin >> M;
+        cin >> P;
+
+        if (M == 299) {
+            cout << 0 << endl;
+            continue;
+
+        }
+        while (M < 300 && P <= 1000) {
+            M++;
+            P += 20;
+            if((P+M)>1000 || M>299) break;
+            cnt++;
+            // cout<<"M is "<<M<<"cnt is"<<cnt<<endl;
+        }
+
+        cout <<cnt<<endl;
+    }
+
+}
